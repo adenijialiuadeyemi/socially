a68454fda6f6e9ebf525b1ed5f13c0d6a48acdb2
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2025-07-03 13:04:01 +0100

    feat: add image upload with uploadthing

---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import { XIcon } from "lucide-react";
+
+interface ImageUploadProps {
+  onChange: (url: string) => void;
+  value: string;
+  endpoint: "postImage";
+}
+
+function ImageUpload({ endpoint, onChange, value }: ImageUploadProps) {
+  if (value) {
+    return (
+      <div className="relative size-40">
+        <img src={value} alt="Upload" className="rounded-md size-40 object-cover" />
+        <button
+          onClick={() => onChange("")}
+          className="absolute top-0 right-0 p-1 bg-red-500 rounded-full shadow-sm"
+          type="button"
+        >
+          <XIcon className="h-4 w-4 text-white" />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadDropzone
+      appearance={{
+        button: "bg-blue-600 px-8 py-2 mt-8 text-white hover:bg-blue-600", // see below for the button fix
+        container: "rounded-lg border border-dashed p-4 py-12",
+        label: "text-sm font-medium mb-2 text-blue-500",
+        uploadIcon: "w-14 h-14",
+      }}
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].ufsUrl);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+}
+export default ImageUpload;
